50425870b02aacbad31a0128a654934620020754
evarellapucky <scherer.alexia@gmail.com>
2024-04-08 12:04:57 +0200

    script01

--- script_01.js
+++ script_01.js
@@ -19,3 +19,23 @@ const entrepreneurs = [
   { first: 'Peter', last: 'Thiel', year: 1967 }
 ];
 
+// Creates an array which contains only the the first and last name of the entrepreneurs
+let namelist = [];
+for (let n = 0; n < entrepreneurs.length; n++) {
+  let entrepreneur = entrepreneurs[n];
+  namelist.push(entrepreneur.first + ' ' + entrepreneur.last);
+};
+console.log("liste des entrepreneurs par nom :", namelist);
+
+// Replace the birth year with the age of the entrepreneurs + first and last with firstName and Lastname
+const currentYear = new Date().getFullYear();
+const entrepreneursAge = entrepreneurs.map(entrepreneur => ({
+  firstName: entrepreneur.first,
+  lastName: entrepreneur.last,
+  age: currentYear - entrepreneur.year
+}));
+console.log("âge actuel des entrepreneurs : ", entrepreneursAge)
+
+// Filter entrepreneurs born in the 70's
+let seventies = entrepreneursAge.filter(entrepreneur => entrepreneur.age >= 45 && entrepreneur.age < 54);
+console.log("entrepreneurs nés dans les années 70 :", seventies)
